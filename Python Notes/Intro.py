d9064744f8853f83dd46eed16a9c7234cc813621
Amardeep1615 <bolagani.amardeep1627@gmail.com>
2025-04-18 23:23:02 +0530

    intorduction

---
+++ Python Notes/Intro.py
@@ -0,0 +1,49 @@
+""" Python Intro
+    1.Python Syntax
+    2.Python Comments
+    3.Python Variables
+    4.Python Datatypes
+    5.Python Numbers
+    6.Python Casting
+    7.Python Strings
+    8.Python Booleans
+    9.Python Operators
+    10.Python Lists
+    11.Python Tuples
+    12.Python Sets
+    13.Python Dictionaries
+    14.Python if--else
+    15.Python Match
+    16.Python While Loops
+    17.Python For Loops
+    18.python Functions
+    19.Python Lambda
+    20.Python Arrays
+    21.Python Classes/Objects
+    22.Python Inheritance
+    23.Python Iterators
+    24.Python Polymorphism
+    25.Python Scope
+    26.Python Modules
+    27.Python Dates
+    28.Python Math
+    29.Python JSON
+    30.Python RegEx 
+    31.Python PIP
+    32.Python Try--Except (Exception Handling)
+    33.Python User Input
+    34.Python String Formatting
+    35.Python File Handling
+    
+
+
+
+
+
+
+
+
+
+
+
+"""
